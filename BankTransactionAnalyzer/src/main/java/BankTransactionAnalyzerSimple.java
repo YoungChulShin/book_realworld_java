9922eb59a4157f71d57423e0edb84fa6c7418fe4
YoungChulShin <go1323@gmail.com>
2020-08-26 14:23:35 +0900

    interface를 이용한 결합도 분리

--- BankTransactionAnalyzer/src/main/java/BankTransactionAnalyzerSimple.java
+++ BankTransactionAnalyzer/src/main/java/BankTransactionAnalyzerSimple.java
@@ -1,31 +1,11 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
-
-    private static final String RESOURCES = "src/main/resources/";
-    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
-
     public static void main(final String... args) throws IOException {
 
-        final String fileName = args[0];
-        final Path path = Paths.get(RESOURCES + fileName);
-        final List<String> lines = Files.readAllLines(path);
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
-        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
-        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-
-        collectSummary(bankStatementProcessor);
-    }
-
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
-        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
-        System.out.println("Transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
-        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
     }
 }
