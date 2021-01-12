16e4c1c3f0c2f650df2c89c7eab6928f1dc1de0c
YoungChulShin <go1323@gmail.com>
2021-01-12 09:56:16 +0900

    Notification Pattern을 이용한 에러 처리

--- BankTransactionAnalyzer2/src/main/java/BankStatementCSVParser.java
+++ BankTransactionAnalyzer2/src/main/java/BankStatementCSVParser.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementCSVParser implements BankStatementParser{
+
+    private static final int EXPECTED_ATTRIBUTES_LENGTH = 3;
     private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     public BankTransaction parseFrom(final String line) {
         final String[] columns = line.split(",");
+        if (columns.length < EXPECTED_ATTRIBUTES_LENGTH) {
+            throw new CSVSyntaxException();
+        }
+
+        OverlySpecificBankStatementValidator validator = new OverlySpecificBankStatementValidator(
+                columns[2], columns[0], columns[1]
+        );
+        Notification notification = validator.validate();
+        if (notification.hasErrors()) {
+            // do somethig
+            return null;
+        }
 
         final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
         final double amount = Double.parseDouble(columns[1]);
         final String description = columns[2];
+
+
 
         return new BankTransaction(date, amount, description);
     }
